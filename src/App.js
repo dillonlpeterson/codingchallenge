d8bc454f7ec02c50fa44aeee7e89a505c3defedf
Dillon Peterson <dillonpeterson@mac.com>
2019-04-13 18:24:47 -0500

    Pass same columns for organizations but hide them

--- src/App.js
+++ src/App.js
@@ -5,44 +5,68 @@ import entities from './data/data.json';
 import MUIDataTable from "mui-datatables";
 
 const personColumns = [{
-    name: 'person_name',
-    label: 'Name', 
-  }, {
-    name: 'person_title', 
-    label: 'Title'
-  }, {
-    name: 'organization_name', 
-    label: 'Organization'
-  }, {
-    name: 'location', 
-    label: 'Location',
-  }];
+  name: 'person_name',
+  label: 'Name',
+}, {
+  name: 'person_title',
+  label: 'Title'
+}, {
+  name: 'organization_name',
+  label: 'Organization'
+}, {
+  name: 'location',
+  label: 'Location',
+}];
 
 const orgColumns = [{
-  name: 'organization_name', 
+  name: 'organization_name',
   text: '',
 }, {
-  name: 'location', 
-  text: '', 
+  name: 'location',
+  text: '',
 }
 ]
 
 const options = {
   'pagination': false,
-  'print': false, 
-  'download':false, 
+  'print': false,
+  'download': false,
+  'filter': false,
+  'filterType': 'dropdown',
 };
 
+
+
 class App extends Component {
-  
+  // Handles change 
+  handleChange = () => {
+    // Adjust results shown in tables below.
+  }
+  renderNavigationBar = () => {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <div class="container">
+          <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
+        </div>
+      </nav>
+
+    )
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2>DLP Coding Challenge</h2>
         </header>
+        {this.renderNavigationBar()}
         <div>
-          <MUIDataTable title={"People"} data={entities} columns={personColumns} options={options}></MUIDataTable>
+          <MUIDataTable
+            title={"People"}
+            data={entities}
+            columns={personColumns}
+            options={options}
+          />
           <MUIDataTable title={"Organizations"} data={entities} columns={orgColumns}></MUIDataTable>
         </div>
       </div>
---
+++ src/EntityTable.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+
+class EntityTable extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        )
+    }
+
+}
