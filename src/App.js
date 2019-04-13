2488fd2cf9df8e6fe8ccbd74ef5775355d9d71ef
Dillon Peterson <dillonpeterson@mac.com>
2019-04-13 13:56:08 -0500

    Have rudimentary table implemented. Shows all person entities from JSON file.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 // Import data from given JSON file.
-import entityData from './data/data.json';
+import entities from './data/data.json';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const personColumns = [{
+    dataField: 'person_name',
+    text: '',
+    align: 'left',
+
+  }, {
+    dataField: 'person_title', 
+    text: '',
+    align: 'left',
+  }, {
+    dataField: 'organization_name', 
+    text: '',
+    align: 'center'
+  }, {
+    dataField: 'location', 
+    text: "",
+    align: 'right',
+  }];
+
+const orgColumns = [{
+  dataField: 'organization_name', 
+  text: '',
+  align: 'left',
+}, {
+  dataField: 'location', 
+  text: '', 
+  aligh: 'right',
+}
+]
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2>DLP Coding Challenge</h2>
         </header>
-        <body>
-          {JSON.stringify(entityData)}
-        </body>
+        <div>
+          <BootstrapTable keyField='id' data={entities} columns={personColumns} search/>
+        </div>
       </div>
     );
   }
